492e3ef94015a2d597e8869b47adc1e341163f36
Николай <nnm_58@mail.ru>
2023-09-30 13:55:55 +0300

    Создал папку Task24

---
+++ Task28/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+Задача 28: Напишите программу, которая
+принимает на вход число N и выдаёт
+произведение чисел от 1 до N.
+4 -> 24
+5 -> 120
+
