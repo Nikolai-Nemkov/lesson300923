5fbecd000dca3aefb253b12e40f28dfc5932640b
Николай <nnm_58@mail.ru>
2023-09-30 14:09:19 +0300

    Создал папку Task28

--- Task27/Program.cs
+++ Task27/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
+
 // Задача 27: Напишите программу, которая принимает на
 // вход число и выдаёт сумму цифр в числе.
 // 452 -> 11
