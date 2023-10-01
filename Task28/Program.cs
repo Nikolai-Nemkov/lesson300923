246243e8741e2c3f0b6d8d02cd7bab46fe2c894a
Николай <nnm_58@mail.ru>
2023-10-01 13:07:44 +0300

    Создал папку Task28

--- Task28/Program.cs
+++ Task28/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Задача № 28");
 
 
 // Задача 28: Напишите программу, которая
